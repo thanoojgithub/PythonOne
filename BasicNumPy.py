67065b0cac04f098bbc9f7688ebd0d8cf5b160b6
thanooj kalathuru <javaeecub@gmail.com>
2020-11-18 23:11:08 +0530

    Update BasicNumPy.py

--- BasicNumPy.py
+++ BasicNumPy.py
@@ -3,3 +3,11 @@
 #There are two basic types of reference locality – temporal and spatial locality. 
 #Temporal locality refers to the reuse of specific data, and/or resources, within a relatively small time duration. 
 #Spatial locality (also termed data locality[3]) refers to the use of data elements within relatively close storage locations. Sequential locality, a special case of spatial locality, occurs when data elements are arranged and accessed linearly, such as, traversing the elements in a one-dimensional array. 
+
+#NumPy stands for Numerical Python.
+#NumPy is a python library used for working with arrays.
+#In Python we have lists that serve the purpose of arrays, but they are slow to process.
+#NumPy aims to provide an array object that is up to 50x faster that traditional Python lists.
+#The array object in NumPy is called ndarray, it provides a lot of supporting functions that make working with ndarray very easy.
+#NumPy arrays are stored at one continuous place in memory unlike lists, so processes can access and manipulate them very efficiently.
+#NumPy is a Python library and is written partially in Python, but most of the parts that require fast computation are written in C or C++.
