b1d4876abf5bbb54b0468522ac94b8945c284e1a
thanooj kalathuru <javaeecub@gmail.com>
2020-11-18 23:23:38 +0530

    Update BasicNumPy.py

--- BasicNumPy.py
+++ BasicNumPy.py
@@ -11,3 +11,40 @@
 #The array object in NumPy is called ndarray, it provides a lot of supporting functions that make working with ndarray very easy.
 #NumPy arrays are stored at one continuous place in memory unlike lists, so processes can access and manipulate them very efficiently.
 #NumPy is a Python library and is written partially in Python, but most of the parts that require fast computation are written in C or C++.
+
+#To create an ndarray, we can pass a list, tuple or any array-like object into the array() method, and it will be converted into an ndarray.
+
+import numpy as np
+import time
+
+a = np.random.rand(1000000)
+b = np.random.rand(1000000)
+
+tic = time.time()
+c = np.dot(a, b)
+print(c)
+toc = time.time()
+
+print("Vectorised version: " + str(1000*(toc-tic)) + " ms")
+
+c = 0
+tic = time.time()
+for i in range(1000000):
+    c += a[i] * b[i]
+toc = time.time()
+print(c)
+print("For loop: " + str(1000*(toc-tic)) + " ms")
+
+arr = np.array(42)
+print(arr)
+print(arr.ndim)
+arr = np.array([1, 2, 3, 4, 5])
+print(arr)
+print(arr.ndim)
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+print(arr)
+print(arr.ndim)
+arr = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+print(arr)
+print(arr.ndim)
+print(type(arr))
