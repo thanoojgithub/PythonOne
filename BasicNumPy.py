5d085bd3003c8c402b0dd4783cd8b3b3d43309e8
thanooj kalathuru <javaeecub@gmail.com>
2020-11-18 22:02:39 +0530

    Create BasicNumPy.py

---
+++ BasicNumPy.py
@@ -0,0 +1,5 @@
+#Locality of Reference
+#In computer science, locality of reference, also known as the principle of locality
+#There are two basic types of reference locality – temporal and spatial locality. 
+#Temporal locality refers to the reuse of specific data, and/or resources, within a relatively small time duration. 
+#Spatial locality (also termed data locality[3]) refers to the use of data elements within relatively close storage locations. Sequential locality, a special case of spatial locality, occurs when data elements are arranged and accessed linearly, such as, traversing the elements in a one-dimensional array. 
